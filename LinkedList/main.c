bb54fc7b6ca03e33c0a376a15a67c3f9185ce35c
Inncrewin <aditya.kumar@inncrewin.com>
2023-12-03 03:13:59 +0530

    Initial Commit

---
+++ LinkedList/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  LinkedList
+//
+//  Created by inncrewin on 03/12/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
